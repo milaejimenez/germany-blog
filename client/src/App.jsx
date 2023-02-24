b1bb65a7460b1e11ef8d62aa1fa793f7776d940f
milaejimenez <milaejimenez@gmail.com>
2023-02-24 16:35:52 +0100

    Create routers for each page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,46 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Router
+} from "react-router-dom";
 import './App.css'
 
+//Import pages
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Single from "./pages/Single";
+import Write from "./pages/Write";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>
+  },
+  {
+    path: "/register",
+    element: <Register/>
+  },
+  {
+    path: "/login",
+    element: <Login/>
+  },
+  {
+    path: "/single",
+    element: <Single/>
+  },
+  {
+    path: "/write",
+    element: <Write/>
+  },
+])
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      Hello world!
+      <RouterProvider router={router} />
     </div>
   )
 }
