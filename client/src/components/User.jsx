ba6a1f284f6765687bb6f53250fec012ca584fd1
milaejimenez <milaejimenez@gmail.com>
2023-02-26 10:24:20 +0100

    Start single post page with user component

---
+++ client/src/components/User.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const User = () => {
+  return (
+    <div className='user'>
+        <img src="" alt="user-image" />
+        <div className='info'>
+            <span>John</span>
+            <p>Post 2 days ago</p>
+        </div>
+    </div>
+  )
+}
+
+export default User
--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import User from '../components/User'
 
 const Single = () => {
   return (
-    <div>Single</div>
+    <div className='single'>
+      <div className="content">Content
+        <User />
+      </div>
+      <div className="sidebar">Sidebar</div>
+    </div>
   )
 }
 
